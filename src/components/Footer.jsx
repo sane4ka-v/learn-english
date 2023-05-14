6fede2850e2d2e43d65a89741951cc8a8bf28fbe
Timentil <artemakarpoff@gmail.com>
2023-05-14 15:33:54 +0500

    delete react-logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,7 @@ import logo from './../logo.svg';
 const Footer = () => {
   return (
     <footer className='App-footer'>
-      <img src={logo} className="App-logo-mini" alt="logo" />
       follow us on any social media (or black meza XD)
-      <img src={logo} className="App-logo-mini" alt="logo" />
     </footer>
   );
 }
